d9ef588845b75efae9501c2ed7c3c031152090bc
grinya0101 <grinya1994015@gmail.com>
2022-10-08 16:48:11 +0300

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,117 @@
-export const App = () => {
+import {Feedback, ButtonContainer, } from 'components/Feedback/Feedback'
+import Statistics from 'components/Statistics/Statistics'
+
+import Notification from 'components/Notification/notification'
+import Section from 'components/Section/Section'
+import { useState } from 'react'
+
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const choiceState = (propertyName) => {
+    switch (propertyName){
+      case "good":
+        setGood(prev => prev + 1);
+        break;
+      case "neutral":
+        setNeutral(prev => prev + 1);
+        break;
+      case "bad":
+        setBad(prev => prev + 1);
+        break;
+        default:
+          return;
+    }
+      };
+
+
+      const totalFeedback = () =>{
+        return good + neutral + bad
+       };
+      
+      const feedbackPercentage = ()=>{
+       return Math.round((good / totalFeedback()) * 100)
+       };
+      
+    
+ 
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  <>
+  <div>
+  <Section title="Please leave feedback">
+   <Feedback 
+   options={["good", "neutral", "bad"]} 
+  choiceState={choiceState}
+  />
+
+  </Section>
+   <Section title="Statistics">
+   {totalFeedback() ?
+     <Statistics good={good} 
+            neutral={neutral} 
+            bad={bad} 
+            total={totalFeedback()} 
+            positivePercentage={feedbackPercentage()}/>
+            
+            :
+            <Notification message="There is no feedback"/>
+          }
+    </Section>
+  </div>
+  </>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// export class App extends Component {
+  
+
+//   /* выбор отзыва гуд ... */
+
+
+
+ 
+
+
+// totalFeedback(){
+//   return this.state.good + this.state.neutral + this.state.bad
+//  };
+
+//  feedbackPercentage(){
+//  return Math.round((this.state.good / this.totalFeedback()) * 100)
+//  };
+
+//  render(){
+//   const options = ['good', 'neutral', 'bad'];
+ 
+// };
+
+// }
+ 
+
+  
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+
+export const Feedback = ({ options, choiceState }) => {
+  return (
+    <>
+      {options.map(option => {
+        return (
+          <span key={option}>
+           <button type="button"
+              name={option}
+              onClick={() => {
+                choiceState(option);
+              }}
+            >
+              {option}</button>
+          
+          </span>
+        );
+      })}
+    </>
+  );
+};
+  
+    
+export default Feedback;
+
+
+
+
+Feedback.propTypes = {
+  choiceGoodq: PropTypes.func,
+  choiceNeutralq: PropTypes.func,
+  choiceBadq: PropTypes.func,
+  
+}
